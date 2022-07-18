aed1e12ba147ed35345d496d3a95113b89ce90a9
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-18 19:11:43 +0530

    added recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[]
+  recipes: Recipe[]=[
+    new Recipe('Test Recipe','This is test recipe','https://cdn.pixabay.com/photo/2017/11/16/18/51/kagyana-2955466__340.jpg')
+  ];
 
   constructor() { }
 
