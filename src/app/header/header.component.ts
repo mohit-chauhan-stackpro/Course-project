b448c31c592b40ff0835579fe5b82d537694aabe
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-20 13:49:45 +0530

    added navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,14 +6,10 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected=new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSelect(feature:string){
-    this.featureSelected.emit(feature)
-  }
 }
