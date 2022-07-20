995ea4a66d1f3dc735b5f937eb98f026c9f4568f
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-20 15:44:42 +0530

    added routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -10,21 +10,25 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe;
-  id!: number;
+  index!: number;
 
   constructor(private recipeService: RecipeService,
-              private route:ActivatedRoute) {
+              private route:ActivatedRoute,private router:Router) {
 
               }
 
   ngOnInit() {
     this.route.params.subscribe((params:Params)=>{
-        this.id=+params['id']
-        this.recipe=this.recipeService.getRecipe(this.id)
+        this.index=+params['id']
+        this.recipe=this.recipeService.getRecipe(this.index)
     })
   }
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo:this.route})
   }
 
 }
