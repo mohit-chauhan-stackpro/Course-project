c0e185fed75a2c676a167d94b0d21f5ac4c879b0
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-20 13:42:52 +0530

    setted up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edi
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directives';
 import { ShoppingListService } from './shoping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ShoppingListService } from './shoping-list/shopping-list.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
